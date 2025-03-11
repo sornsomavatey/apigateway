6fecf61ac45ed1c8c6fae934a0e2f2a09ba00e21
Sorn Somavatey <156301805+sornsomavatey@users.noreply.github.com>
2025-03-11 12:29:56 +0700

    Update apiGateway.js

--- apiGateway.js
+++ apiGateway.js
@@ -15,7 +15,7 @@ app.post('/userRegister/register', (req, res, next) => {
     // console.log("Request body:", req.body);
     // console.log("Forwarding to /register...");
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.REGISTER_PORT}`, {
+}, proxy(`http://34.239.134.251:${process.env.REGISTER_PORT}`, {
     proxyReqPathResolver: (req) => '/register',
     userResDecorator: (proxyRes, proxyResData) => {
         // console.log("INSIDE API GATEWAY USER REGISTER");
@@ -26,7 +26,7 @@ app.post('/userRegister/register', (req, res, next) => {
 
 app.post('/auth/login', (req, res, next) => {
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.LOGIN_PORT}`, {
+}, proxy(`http://34.239.134.251:${process.env.LOGIN_PORT}`, {
     proxyReqPathResolver: (req) => '/login',
     userResDecorator: (proxyRes, proxyResData) => {
         return proxyResData;
@@ -38,7 +38,7 @@ app.post('/auth/login', (req, res, next) => {
 // for user
 app.use('/userCreate/blog/create', authenticate, (req, res, next) => {
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.CREATE_BLOG_PORT}`, {
+}, proxy(`http://34.239.134.251:${process.env.CREATE_BLOG_PORT}`, {
     proxyReqPathResolver: (req) => '/blog/create',
     userResDecorator: (proxyRes, proxyResData) => {
         return proxyResData;
@@ -47,7 +47,7 @@ app.use('/userCreate/blog/create', authenticate, (req, res, next) => {
 
 app.use('/userView/blog/view/:id', authenticate, (req, res, next) => {
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.VIEW_BLOG_PORT}`, {
+}, proxy(`http://34.239.134.251:${process.env.VIEW_BLOG_PORT}`, {
     proxyReqPathResolver: (req) => '/blog/view/:id',
     userResDecorator: (proxyRes, proxyResData) => {
         return proxyResData;
@@ -57,7 +57,7 @@ app.use('/userView/blog/view/:id', authenticate, (req, res, next) => {
 // for admin
 app.use('/adminUpdate/blog/update/:id', authenticate, authRole('admin'), (req, res, next) => {
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.UPDATE_BLOG_PORT}`, {
+}, proxy(`http://3.84.18.143:${process.env.UPDATE_BLOG_PORT}`, {
     proxyReqPathResolver: (req) => '/blog/update/:id',
     userResDecorator: (proxyRes, proxyResData) => {
         return proxyResData;
@@ -66,7 +66,7 @@ app.use('/adminUpdate/blog/update/:id', authenticate, authRole('admin'), (req, r
 
 app.use('/adminDelete/blog/admin/delete/:id', authenticate, authRole('admin'), (req, res, next) => {
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.DELETE_BLOG_PORT}`, {
+}, proxy(`http://3.84.18.143:${process.env.DELETE_BLOG_PORT}`, {
     proxyReqPathResolver: (req) => '/blog/admin/delete/:id',
     userResDecorator: (proxyRes, proxyResData) => {
         return proxyResData;
@@ -76,7 +76,7 @@ app.use('/adminDelete/blog/admin/delete/:id', authenticate, authRole('admin'), (
 
 app.use('/adminView/blog/admin/all', authenticate, authRole('admin'), (req, res, next) => {
     next(); // Let the proxy middleware handle it
-}, proxy(`http://localhost:${process.env.ADMIN_VIEW_PORT}`, {
+}, proxy(`http://3.84.18.143:${process.env.ADMIN_VIEW_PORT}`, {
     proxyReqPathResolver: (req) => '/blog/admin/all', 
     userResDecorator: (proxyRes, proxyResData) => {
         return proxyResData;
